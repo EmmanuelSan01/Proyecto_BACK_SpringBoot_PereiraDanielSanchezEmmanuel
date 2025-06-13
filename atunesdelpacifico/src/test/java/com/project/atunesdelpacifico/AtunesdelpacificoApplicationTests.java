e921f7dc3189a0af68ab82ba396edb6f4e897f31
Emmanuel Sánchez <114524605+EmmanuelSan01@users.noreply.github.com>
2025-06-13 23:15:46 +0200

    Created maven project

---
+++ atunesdelpacifico/src/test/java/com/project/atunesdelpacifico/AtunesdelpacificoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.atunesdelpacifico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtunesdelpacificoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
